52e14e69f8ff696a90e7263230f40d42ad9cc46b
Michael Lo <lomic8@gmail.com>
2024-06-15 12:56:05 -0700

    Added the EOS token as the padding token

--- models/baseline_model.py
+++ models/baseline_model.py
@@ -57,6 +57,10 @@ def main():
     texts = df['text'].tolist()
     train_texts, val_texts = train_test_split(texts, test_size=0.1)
     tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+
+	# Set the padding token to the EOS token
+	tokenizer.pad_token = tokenizer.eos_token
+
     train_dataset = CustomDataset(train_texts, tokenizer, max_len=128)
     val_dataset = CustomDataset(val_texts, tokenizer, max_len=128)
 
