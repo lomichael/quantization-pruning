bfa7fa7ac7ed4a44a85a720c16726d865327d57e
Michael Lo <lomic8@gmail.com>
2024-06-17 17:18:20 -0700

    Added path resolution for imports to quantization and pruning scripts

--- quantization/post_training_quantization.py
+++ quantization/post_training_quantization.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import torch
 import torch.quantization
 from transformers import GPT2LMHeadModel, GPT2Tokenizer
