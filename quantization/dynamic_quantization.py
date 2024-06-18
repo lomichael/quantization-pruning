f3489d64f5f06cf3ff499ef0aa0c047f869b5628
Michael Lo <lomic8@gmail.com>
2024-06-17 19:05:53 -0700

    Added path resolution for imports to dynamic quantization script

--- quantization/dynamic_quantization.py
+++ quantization/dynamic_quantization.py
@@ -1,3 +1,8 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+
 import torch
 from transformers import GPT2LMHeadModel, GPT2Tokenizer
 from torch.utils.data import DataLoader
