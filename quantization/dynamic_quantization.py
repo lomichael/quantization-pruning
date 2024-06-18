8337481795d6d4716b2cacca7bb764d9e0e405e9
Michael Lo <lomic8@gmail.com>
2024-06-17 22:31:14 -0700

    Semi-reverted back to the quantization that actually ran, but now with verification and more logging

--- quantization/dynamic_quantization.py
+++ quantization/dynamic_quantization.py
@@ -7,7 +7,7 @@ from transformers import GPT2LMHeadModel, GPT2Tokenizer
 from torch.utils.data import DataLoader
 import pandas as pd
 from datasets.custom_dataset import CustomDataset
-from evaluation.evaluation_utils import evaluate, measure_model_size, measure_inference_time
+from utils.evaluation_utils import evaluate, measure_model_size, measure_inference_time
 import logging
 from tqdm import tqdm
 
